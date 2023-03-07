5452c44ecc08c8afe1a06a6a538f34e20d12be8d
jess <andiejessica2020@gmail.com>
2023-03-07 07:35:23 +0300

    ajout Mapping et attributs

---
+++ src/main/java/etu1938/framework/Mapping.java
@@ -0,0 +1,26 @@
+package etu1938.framework;
+
+public class Mapping {
+    String className;
+    String Method;
+    public Mapping()
+    {
+
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getMethod() {
+        return Method;
+    }
+
+    public void setMethod(String method) {
+        Method = method;
+    }
+}
