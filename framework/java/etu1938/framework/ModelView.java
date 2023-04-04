041eae306e2d773ecf2751f97a6045363fbd8592
Andiiiiie <andiejessica2020@gmail.com>
2023-04-04 13:08:56 +0300

    sprint6: envoie objet

---
+++ framework/java/etu1938/framework/ModelView.java
@@ -0,0 +1,34 @@
+package etu1938.framework;
+
+import java.util.HashMap;
+
+public class ModelView {
+    String view;
+    HashMap<String, Object> data;
+
+    public ModelView(String view) {
+        setView(view);
+        setData(new HashMap<>());
+    }
+
+    public void addItem(String key,Object value)
+    {
+        this.getData().put(key,value);
+    }
+
+    public String getView() {
+        return view;
+    }
+
+    public void setView(String view) {
+        this.view = view;
+    }
+
+    public HashMap<String, Object> getData() {
+        return data;
+    }
+
+    public void setData(HashMap<String, Object> data) {
+        this.data = data;
+    }
+}
