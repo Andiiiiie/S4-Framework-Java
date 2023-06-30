2589087d15a850460ee567605ec934c07de0efb6
Andiiiiie <andiejessica2020@gmail.com>
2023-06-30 16:32:50 +0300

    sprint15: delete sessions

--- framework/java/etu1938/framework/core/ModelView.java
+++ framework/java/etu1938/framework/core/ModelView.java
@@ -1,6 +1,8 @@
 package etu1938.framework.core;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class ModelView {
     String view;
@@ -8,6 +10,10 @@ public class ModelView {
     HashMap<String, Object> session;
 
     Boolean isJson=false;
+
+    Boolean InvalidateSession=false;
+
+    List<String> removeSession=new ArrayList<>();
 
     public ModelView(String view) {
         setView(view);
@@ -20,10 +26,11 @@ public class ModelView {
         this.getData().put(key,value);
     }
 
-    public void deleteSession(String key)
+    public void addSessionToRemove(String key)
     {
-        addSession(key,null);
+        getRemoveSession().add(key);
     }
+
 
     public void addSession(String key,Object value)
     {
@@ -62,5 +69,21 @@ public class ModelView {
 
     public void setJson(Boolean json) {
         isJson = json;
+    }
+
+    public Boolean getInvalidateSession() {
+        return InvalidateSession;
+    }
+
+    public void setInvalidateSession(Boolean invalidateSession) {
+        InvalidateSession = invalidateSession;
+    }
+
+    public List<String> getRemoveSession() {
+        return removeSession;
+    }
+
+    public void setRemoveSession(List<String> removeSession) {
+        this.removeSession = removeSession;
     }
 }
